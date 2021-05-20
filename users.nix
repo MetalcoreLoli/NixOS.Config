0ed54f6e81b9b9f8889ff6b1f23278551503f57d
123@123/123 <123@123/123>
2021-05-20 23:01:03 +0300

    move users to sep file

---
+++ users.nix
@@ -0,0 +1,11 @@
+{ ... }:
+{
+  # define a user account
+  users.users.daniel = {
+	isNormalUser = true;
+	createHome = true;
+	home = "/home/daniel";
+	description = "Daniel Freedman";
+	extraGroups = [ "wheel" "networkmanager" ];
+  };
+}
