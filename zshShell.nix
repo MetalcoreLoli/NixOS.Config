40228b992c1639087507b6c50723522fbf1ffac7
MetalcoreLoli <deadpool1118@gmail.com>
2021-05-21 21:43:49 +0300

    change something in zsh config

--- zshShell.nix
+++ zshShell.nix
@@ -1,15 +1,16 @@
 { pkgs, ... }:
 {
+
 	programs.zsh.enable = true;
+        programs.zsh.ohMyZsh.customPkgs = [
+		pkgs.nix-zsh-completions
+		pkgs.zsh-autosuggestions
+		pkgs.zsh-syntax-highlighting
+	];
 	programs.zsh.ohMyZsh = {
  		enable  = true;
 		plugins = [ "git" "cargo" ];
 		theme   = "lambda"; 
         };
 
-        programs.zsh.ohMyZsh.customPkgs = [
-		pkgs.nix-zsh-completions
-		pkgs.zsh-autosuggestions
-		pkgs.zsh-syntax-highlighting
-	];
 }
